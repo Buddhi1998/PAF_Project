0a8da8972a2ec7e744e29a178804214dc41aa0d8
Amindi99 <amindiperera1@gmail.com>
2021-04-20 22:37:48 +0530

    insert

--- IT19015422_Amindi/src/model/FundDB.java
+++ IT19015422_Amindi/src/model/FundDB.java
@@ -20,4 +20,41 @@ public class FundDB {
 		
 		return con; 
 	} 
+	
+	public String insertFund(String funderName, String fundAmount, String fundDate,String funderLocation) 
+	{ 
+		String output = ""; 
+		try
+		{ 
+			Connection con = connect(); 
+			if (con == null) 
+			{
+				return "Error while connecting to the database for inserting."; 
+			} 
+			
+			// create a prepared statement
+			String query = " insert into fund_management (`funderID`,`funderName`,`fundAmount`,`fundDate`,`funderLocation`)" + " values (?, ?, ? ,? ,?)";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query); 
+			
+			// binding values
+			preparedStmt.setInt(1, 0); 
+			preparedStmt.setString(2, funderName); 
+			preparedStmt.setDouble(3, Double.parseDouble(fundAmount)); 
+			preparedStmt.setString(4, fundDate);
+			preparedStmt.setString(5, funderLocation); 
+			
+			// execute the statement
+			preparedStmt.execute(); 
+			con.close(); 
+			output = "Inserted successfully"; 
+		} 
+		catch (Exception e) 
+		{ 
+			output = "Error while inserting the funding details."; 
+			System.err.println(e.getMessage()); 
+		} 
+		return output; 
+	} 
+	
 }
