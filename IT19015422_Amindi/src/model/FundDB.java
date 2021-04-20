7079831bf90a428a965488e2eec1476ffc6671ca
Amindi99 <amindiperera1@gmail.com>
2021-04-20 22:50:53 +0530

    View funds

--- IT19015422_Amindi/src/model/FundDB.java
+++ IT19015422_Amindi/src/model/FundDB.java
@@ -57,4 +57,64 @@ public class FundDB {
 		return output; 
 	} 
 	
+	public String readFunds() 
+	{ 
+		String output = ""; 
+		
+		try
+		{ 
+			Connection con = connect(); 
+			if (con == null) 
+			{return "Error while connecting to the database for reading."; } 
+			
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>funderName</th><th>fundAmount</th>" +
+					"<th>fundDate</th>" + 
+					"<th>funderLocation</th>" +
+					"<th>Update</th><th>Remove</th></tr>"; 
+ 
+			String query = "select * from fund_management"; 
+			Statement stmt = con.createStatement(); 
+			ResultSet rs = stmt.executeQuery(query); 
+			
+			// iterate through the rows in the result set
+			while (rs.next()) 
+			{ 
+				String funderID = Integer.toString(rs.getInt("funderID")); 
+				String funderName = rs.getString("funderName"); 
+				String fundAmount = Double.toString(rs.getDouble("fundAmount")); 
+				String fundDate = rs.getString("fundDate"); 
+				String funderLocation = rs.getString("funderLocation"); 
+				
+				// Add into the html table
+				output += "<tr><td>" + funderID + "</td>"; 
+				output += "<td>" + funderName + "</td>"; 
+				output += "<td>" + fundAmount + "</td>"; 
+				output += "<td>" + fundDate + "</td>"; 
+				output += "<td>" + funderLocation + "</td>"; 
+				
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+						+ "<td><form method='post' action='funds.jsp'>"
+						+ "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+						+ "<input name='funderID' type='hidden' value='" + funderID 
+						+ "'>" + "</form></td></tr>"; 
+			} 
+			con.close(); 
+			
+			// Complete the html table
+			output += "</table>"; 
+			
+			
+		} 
+		catch (Exception e) 
+		{ 
+			output = "Error while reading the fund details."; 
+			System.err.println(e.getMessage()); 
+			
+		} 
+		return output;
+		 
+	} 
+	
 }
