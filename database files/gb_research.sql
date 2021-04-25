f0b3040610b5cf40225eaba809e4747bf6e36bdb
rasuni99 <68697047+rasuni99@users.noreply.github.com>
2021-04-25 14:02:45 +0530

    Add files via upload

---
+++ database files/gb_research.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2021 at 08:52 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gb_research`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paper`
+--
+
+CREATE TABLE `paper` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `leader` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `campus` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `paper`
+--
+
+INSERT INTO `paper` (`id`, `name`, `description`, `leader`, `date`, `campus`) VALUES
+(1, 'economics', 'supply and demand', 'Sahan', '2021-04-21', 'SLLIT'),
+(2, 'mobile appication', 'how work mobile appilication', 'Ishuka', '2021-04-22', 'NIBM'),
+(11, 'growth', 'country growth', 'Sadun', '2021-04-29', 'NSBM'),
+(15, 'dsa', 'data structures', 'Rashini', '2021-04-23', 'NIBM'),
+(16, 'oop', 'oop cocepts', 'Kasuni', '2021-04-26', 'SLIIT'),
+(20, 'DS', 'jjjj', 'Tharushi', '2021-04-20', 'SLIIT');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `paper`
+--
+ALTER TABLE `paper`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `paper`
+--
+ALTER TABLE `paper`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
